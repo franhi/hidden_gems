143abc5d9e295e972254d328e0ea2a5973bb54c7
Francois Lecendreux <lecendreux.francois@gmail.com>
2018-06-25 17:48:55 +0200

    posts controller and new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,51 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save!
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save!
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :category, :location, :price, :genre, :description, :photo, :status, :begin_date, :end_date)
+  end
+
 end
