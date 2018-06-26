c55f53b07d6faaddf8150637b5d28b6e5a8fb944
Erland Van Reet <erlandvr@gmail.com>
2018-06-26 20:33:00 +0200

    review methods, upvoting and downvoting on review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,6 +22,18 @@ class ReviewsController < ApplicationController
     redirect_to root_path
   end
 
+  def upvote
+    @review = Review.find(params[:id])
+    @review.upvote_by current_user
+    redirect_to post_path(@review.post)
+  end
+
+  def downvote
+    @review = Review.find(params[:id])
+    @review.downvote_by current_user
+    redirect_to post_path(@review.post)
+  end
+
   private
 
   def review_params
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+  acts_as_votable
   belongs_to :post
   belongs_to :user
   has_many :likes
