c94ee4f953915a216bc8526e6531f3adaad8b3b7
Erland Van Reet <erlandvr@gmail.com>
2018-06-28 16:37:13 +0200

    reportsController: new, create, destroy, reviewscontroller: change redirection after destroy, created route to reports/new, created form for report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,32 @@
 class ReportsController < ApplicationController
   def new
+    @post = Post.find(params[:post_id])
+    @report = Report.new
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @report = Report.new(report_params)
+    @report.user_id = current_user.id
+    @report.post = @post
+    if @report.save!
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @report = Review.find(params[:id])
+    @report.delete
+    redirect_to post_path(@post)
+  end
+
+
+private
+
+  def report_params
+    params.require(:report).permit(:description)
   end
 end
