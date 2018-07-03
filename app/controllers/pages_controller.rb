2723847b988f5313d05bf42f5b12ae6334a97f29
Francois Lecendreux <lecendreux.francois@gmail.com>
2018-07-03 17:35:07 +0200

    work in pr geolocation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-
+  skip_before_action :authenticate_user!
   def home
     # @collection = Post.search_post(params[:query])
     # if @collection.empty?
@@ -8,13 +8,19 @@ class PagesController < ApplicationController
       # @posts = Post.search_post(params[:query]).best_posts
     # end
 
+    if !params[:latitude].nil?
+      @coordinates = [params[:latitude], params[:longitude]]
+
+    # @results = Geocoder.search(@coordinates)
+    # @location = @results.coordinates
+      @posts = Post.all.near(@coordinates, 1)
+      raise
+   else
     # @user_location = params[:location]
     # @posts = Post.all.near(@user_location, 10 )
-    @posts = Post.all
-
-    respond_to do |format|
-      format.js { render :js => 'showPosition(position)' }
+      @posts = Post.all
     end
   end
 end
+
 
