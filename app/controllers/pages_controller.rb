1789dfb3ce8f74986804bbbda0950f4ea0080377
Francois Lecendreux <lecendreux.francois@gmail.com>
2018-07-03 11:03:42 +0200

    wip geolocation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,19 @@ class PagesController < ApplicationController
 
   def home
     # @collection = Post.search_post(params[:query])
-      # if @collection.empty?
-        # @posts = Post.best_posts
-      # else
-        # @posts = Post.search_post(params[:query]).best_posts
-      # end
-    @user_location = params[:location]
-    @posts = Post.all.near(@user_location, 10 )
-   end
+    # if @collection.empty?
+      # @posts = Post.best_posts
+    # else
+      # @posts = Post.search_post(params[:query]).best_posts
+    # end
 
-   respond_to do |format|
-    format.js { render :js => 'showPosition(position)' }
+    # @user_location = params[:location]
+    # @posts = Post.all.near(@user_location, 10 )
+    @posts = Post.all
+
+    respond_to do |format|
+      format.js { render :js => 'showPosition(position)' }
+    end
   end
 end
 
