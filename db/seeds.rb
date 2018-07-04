8f78e4f069fd1c897da1ecb297688452a5e1ab43
Irma <irma.kaninda@gmail.com>
2018-07-04 17:46:04 +0200

    seed update for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-  # Report.destroy_all
-  # Review.destroy_all
-  # Favorite.destroy_all
-  # Post.delete_all
-  # User.destroy_all
+  Report.destroy_all
+  Review.destroy_all
+  Favorite.destroy_all
+  TagPost.destroy_all
+  TagUser.destroy_all
+  Tag.destroy_all
+  Post.delete_all
+  User.destroy_all
 
 
 User.create!(email: "irma@irma.com", username: "Irma", password: "lewagon", address: "Cantersteen 10, 1000 Bruxelles")
